f0a8768a7abe0c629cb873e435114944bf2295ab
fool1once <jhonbenedictzulueta@gmail.com>
2026-03-28 22:59:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const elements = document.querySelectorAll('.fade-in');
 
+window.addEventListener('load', () => {
+  elements.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.add('show');
+    }, index * 300);
+  });
+});
